5fa9d002766883c207a5ad3001f84cccbfbcb287
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-05-13 20:20:49 -0400

    (func)added flashcard correct functionality

--- public/app.js
+++ public/app.js
@@ -39,38 +39,35 @@ app.controller('MainController', function($scope) {
         answers: [
             { text: 'A front-end framework for great power!', correct: true },
             { text: 'Something lame, who cares, whatever.', correct: false },
-            { text: 'Some kind of fish, right?', correct: false }
+            { text: 'Some kind of fish, right?', correct: false },
         ]
+        answered: false
     	},
     	{
         question: 'What is a controller?',
         answers: [
             { text: 'Something that manages my front-end routes', correct: false },
             { text: 'A function that allows me to manage a scope', correct: true },
-            { text: 'An Angular template', correct: false }
+            { text: 'An Angular template', correct: false },
         ]
+         answered: false
     	},
     	{
         question: 'What does {{ }} do?',
         answers: [
             { text: 'It runs some Javascript', correct: false },
             { text: 'It looks for variables in HTML', correct: false },
-            { text: 'It runs an Angular expression that accesses my $scope', correct: true }
+            { text: 'It runs an Angular expression that accesses my $scope', correct: true },
         ]
+         answered: false
     	}
 	]
 
-	$scope.answerQuestion = function(theAnswer) {
+	$scope.answerQuestion = function(theAnswer, flashCard) {
 		 
-		console.log(theAnswer); 
-		$scope.answered = function() {
-
-		}
+		
 
 
-	}
-
-	
-
+		console.log(theAnswer.correct); 
 	
 });
