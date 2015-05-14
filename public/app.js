6e155b2c172f094594f212c1b8bf1855daa299ed
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-05-13 20:51:08 -0400

    (func) got flash cards to display color with answer, needed to attribute to individual flash card obj

--- public/app.js
+++ public/app.js
@@ -35,39 +35,53 @@ app.controller('MainController', function($scope) {
 
 	$scope.flashCards =  [
 		{
-        question: 'What is Angular?',
-        answers: [
-            { text: 'A front-end framework for great power!', correct: true },
-            { text: 'Something lame, who cares, whatever.', correct: false },
-            { text: 'Some kind of fish, right?', correct: false },
-        ]
-        answered: false
+	        question: 'What is Angular?',
+	        answered: false,
+	        answers: [
+	            { text: 'A front-end framework for great power!', correct: true },
+	            { text: 'Something lame, who cares, whatever.', correct: false },
+	            { text: 'Some kind of fish, right?', correct: false }
+	        ]
     	},
+
     	{
-        question: 'What is a controller?',
-        answers: [
-            { text: 'Something that manages my front-end routes', correct: false },
-            { text: 'A function that allows me to manage a scope', correct: true },
-            { text: 'An Angular template', correct: false },
-        ]
-         answered: false
+	        question: 'What is a controller?',
+	        answered: false,
+	        answers: [
+	            { text: 'Something that manages my front-end routes', correct: false },
+	            { text: 'A function that allows me to manage a scope', correct: true },
+	            { text: 'An Angular template', correct: false }
+	        ]
     	},
+
     	{
-        question: 'What does {{ }} do?',
-        answers: [
-            { text: 'It runs some Javascript', correct: false },
-            { text: 'It looks for variables in HTML', correct: false },
-            { text: 'It runs an Angular expression that accesses my $scope', correct: true },
-        ]
-         answered: false
+	        question: 'What does {{ }} do?',
+	        answered: false,
+	        answers: [
+	            { text: 'It runs some Javascript', correct: false },
+	            { text: 'It looks for variables in HTML', correct: false },
+	            { text: 'It runs an Angular expression that accesses my $scope', correct: true }
+	        ]
     	}
-	]
+	];
 
 	$scope.answerQuestion = function(theAnswer, flashCard) {
-		 
+		
+		console.log(theAnswer.correct); 
+
+		if(flashCard.answered == false) {
+			flashCard.answered = true; //now they've answered
+
+			//change the color
+			if(theAnswer.correct == true) {
+				flashCard.answeredCorrectly = true;
+			}
+			else {
+				flashCard.answeredCorrectly = false; 
+			}
+		}
+
 		
 
-
-		console.log(theAnswer.correct); 
-	
-});
+	}
+}); 
